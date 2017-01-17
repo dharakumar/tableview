36c63c5a497f5a176f387ab745bd76ce1ba2b488
Admin <admin@Admins-MacBook-Pro-3.local>
2017-01-17 12:44:02 -0800

    Initial Commit

---
+++ eg tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eg tableview
+//
+//  Created by Admin on 1/17/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eg tableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eg tableview
+//
+//  Created by Admin on 1/17/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
